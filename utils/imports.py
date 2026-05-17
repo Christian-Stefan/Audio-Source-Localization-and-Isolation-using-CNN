172e520a6b7a720d3157e1896a5cdcb886343911
Gheorghiu <c.gheorghiu1@student.tue.nl>
2026-05-17 17:32:56 +0200

    <Major> update on multiple plans as ex 2.1 d is solved

--- utils/imports.py
+++ utils/imports.py
@@ -5,3 +5,11 @@ import torch
 import pandas as pd
 import matplotlib.pyplot as PLT
 from torchinfo import summary
+from utils.assignment1_data.mnist_dataloader import create_dataloaders
+import argparse
+from torch.optim import (
+    SGD,RAdam
+)
+from torch.nn import (
+    BCELoss, MSELoss
+)
